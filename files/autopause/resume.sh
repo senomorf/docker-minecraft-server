e391c8a5230f6f44c16de6be0198aafcc9d6435a
Arseny Chuvikin <arseniy.chuvikin@zyfra.com>
2020-11-16 15:53:24 +0300

    replace ps -a with ps -e (see: http://manpages.ubuntu.com/manpages/focal/man1/ps.1.html)

--- files/autopause/resume.sh
+++ files/autopause/resume.sh
@@ -2,7 +2,7 @@
 
 . /start-utils
 
-if [[ $( ps -a -o stat,comm | grep 'java' | awk '{ print $1 }') =~ ^T.*$ ]] ; then
+if [[ $( ps -e -o stat,comm | grep 'java' | awk '{ print $1 }') =~ ^T.*$ ]] ; then
   logAutopauseAction "Knocked, resuming Java process"
   killall -q -CONT java
 fi
